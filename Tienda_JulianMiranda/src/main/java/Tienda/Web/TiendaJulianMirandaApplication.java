b6ef6f9bf81f011de74c795ad65fb13ca5922680
julianmiranmv09 <jmiranda00302@ufide.ac.cr>
2025-05-21 19:15:07 -0600

    Commit 1
    Mi primer commit

---
+++ Tienda_JulianMiranda/src/main/java/Tienda/Web/TiendaJulianMirandaApplication.java
@@ -0,0 +1,13 @@
+package Tienda.Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaJulianMirandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaJulianMirandaApplication.class, args);
+	}
+
+}
---
+++ Tienda_JulianMiranda/src/test/java/Tienda/Web/TiendaJulianMirandaApplicationTests.java
@@ -0,0 +1,13 @@
+package Tienda.Web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaJulianMirandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
